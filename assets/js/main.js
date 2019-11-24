5d45dd3ace36dbf6b2a1b4e50eeed418870cd502
Riley <riley.marcoux@gmail.com>
2019-11-23 22:25:58 -0500

    New website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,104 +1,352 @@
 /*
-	Snapshot by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+ Multiverse by HTML5 UP
+ html5up.net | @ajlkn
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
-(function($) {
+ Added EXIF data and enhanced for Jekyll by Ram Patra
+ */
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
-	});
+(function ($) {
 
-	$(function() {
+    skel.breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small: '(max-width: 736px)',
+        xsmall: '(max-width: 480px)'
+    });
 
-		var	$window = $(window),
-			$body = $('body');
+    $(function () {
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        var $window = $(window),
+            $body = $('body'),
+            $wrapper = $('#wrapper');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        // Hack: Enable IE workarounds.
+        if (skel.vars.IEVersion < 12)
+            $body.addClass('ie');
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Touch?
+        if (skel.vars.mobile)
+            $body.addClass('touch');
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Transitions supported?
+        if (skel.canUse('transition')) {
 
-		// Scrolly.
-			$('.scrolly').scrolly();
+            // Add (and later, on load, remove) "loading" class.
+            $body.addClass('loading');
 
-		// Gallery.
-			$('.gallery').each(function() {
+            $window.on('load', function () {
+                window.setTimeout(function () {
+                    $body.removeClass('loading');
+                }, 100);
+            });
 
-				var	$gallery = $(this),
-					$content = $gallery.find('.content');
+            // Prevent transitions/animations on resize.
+            var resizeTimeout;
 
-				// Poptrox.
-					$content.poptrox({
-						usePopupCaption: true
-					});
+            $window.on('resize', function () {
 
-				// Tabs.
-					$gallery.each( function() {
+                window.clearTimeout(resizeTimeout);
 
-						var $this = $(this),
-							$tabs = $this.find('.tabs a'),
-							$media = $this.find('.media');
+                $body.addClass('resizing');
 
-						$tabs.on('click', function(e) {
+                resizeTimeout = window.setTimeout(function () {
+                    $body.removeClass('resizing');
+                }, 100);
 
-							var $this = $(this),
-								tag = $this.data('tag');
+            });
 
-							// Prevent default.
-							 	e.preventDefault();
+        }
 
-							// Remove active class from all tabs.
-								$tabs.removeClass('active');
+        // Scroll back to top.
+        $window.scrollTop(0);
 
-							// Reapply active class to current tab.
-								$this.addClass('active');
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-							// Hide media that do not have the same class as the clicked tab.
-								$media
-									.fadeOut('fast')
-									.each(function() {
+        // Panels.
+        var $panels = $('.panel');
 
-										var $this = $(this);
+        $panels.each(function () {
 
-										if ($this.hasClass(tag))
-											$this
-												.fadeOut('fast')
-												.delay(200)
-												.queue(function(next) {
-													$this.fadeIn();
-													next();
-												});
+            var $this = $(this),
+                $toggles = $('[href="#' + $this.attr('id') + '"]'),
+                $closer = $('<div class="closer" />').appendTo($this);
 
-									});
+            // Closer.
+            $closer
+                .on('click', function (event) {
+                    $this.trigger('---hide');
+                });
 
-						});
+            // Events.
+            $this
+                .on('click', function (event) {
+                    event.stopPropagation();
+                })
+                .on('---toggle', function () {
 
-					});
+                    if ($this.hasClass('active'))
+                        $this.triggerHandler('---hide');
+                    else
+                        $this.triggerHandler('---show');
 
+                })
+                .on('---show', function () {
 
-			});
+                    // Hide other content.
+                    if ($body.hasClass('content-active'))
+                        $panels.trigger('---hide');
 
-	});
+                    // Activate content, toggles.
+                    $this.addClass('active');
+                    $toggles.addClass('active');
+
+                    // Activate body.
+                    $body.addClass('content-active');
+
+                })
+                .on('---hide', function () {
+
+                    // Deactivate content, toggles.
+                    $this.removeClass('active');
+                    $toggles.removeClass('active');
+
+                    // Deactivate body.
+                    $body.removeClass('content-active');
+
+                });
+
+            // Toggles.
+            $toggles
+                .removeAttr('href')
+                .css('cursor', 'pointer')
+                .on('click', function (event) {
+
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    $this.trigger('---toggle');
+
+                });
+
+        });
+
+        // Global events.
+        $body
+            .on('click', function (event) {
+
+                if ($body.hasClass('content-active')) {
+
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    $panels.trigger('---hide');
+
+                }
+
+            });
+
+        $window
+            .on('keyup', function (event) {
+
+                if (event.keyCode == 27
+                    && $body.hasClass('content-active')) {
+
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    $panels.trigger('---hide');
+
+                }
+
+            });
+
+        // Header.
+        var $header = $('#header');
+
+        // Links.
+        $header.find('a').each(function () {
+
+            var $this = $(this),
+                href = $this.attr('href');
+
+            // Internal link? Skip.
+            if (!href
+                || href.charAt(0) == '#')
+                return;
+
+            // Redirect on click.
+            $this
+                .removeAttr('href')
+                .css('cursor', 'pointer')
+                .on('click', function (event) {
+
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    window.location.href = href;
+
+                });
+
+        });
+
+        // Footer.
+        var $footer = $('#footer');
+
+        // Copyright.
+        // This basically just moves the copyright line to the end of the *last* sibling of its current parent
+        // when the "medium" breakpoint activates, and moves it back when it deactivates.
+        $footer.find('.copyright').each(function () {
+
+            var $this = $(this),
+                $parent = $this.parent(),
+                $lastParent = $parent.parent().children().last();
+
+            skel
+                .on('+medium', function () {
+                    $this.appendTo($lastParent);
+                })
+                .on('-medium', function () {
+                    $this.appendTo($parent);
+                });
+
+        });
+
+        // Main.
+        var $main = $('#main'),
+            exifDatas = {};
+
+        // Thumbs.
+        $main.children('.thumb').each(function () {
+
+            var $this = $(this),
+                $image = $this.find('.image'), $image_img = $image.children('img'),
+                x;
+
+            // No image? Bail.
+            if ($image.length == 0)
+                return;
+
+            // Image.
+            // This sets the background of the "image" <span> to the image pointed to by its child
+            // <img> (which is then hidden). Gives us way more flexibility.
+
+            // Set background.
+            $image.css('background-image', 'url(' + $image_img.attr('src') + ')');
+
+            // Set background position.
+            if (x = $image_img.data('position'))
+                $image.css('background-position', x);
+
+            // Hide original img.
+            $image_img.hide();
+
+            // Hack: IE<11 doesn't support pointer-events, which means clicks to our image never
+            // land as they're blocked by the thumbnail's caption overlay gradient. This just forces
+            // the click through to the image.
+            if (skel.vars.IEVersion < 11)
+                $this
+                    .css('cursor', 'pointer')
+                    .on('click', function () {
+                        $image.trigger('click');
+                    });
+
+            // EXIF data					
+            EXIF.getData($image_img[0], function () {
+                exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
+            });
+
+        });
+
+        // Poptrox.
+        $main.poptrox({
+            baseZIndex: 20000,
+            caption: function ($a) {
+                var $image_img = $a.children('img');
+                var data = exifDatas[$image_img.data('name')];
+                if (data === undefined) {
+                    // EXIF data					
+                    EXIF.getData($image_img[0], function () {
+                        data = exifDatas[$image_img.data('name')] = getExifDataMarkup(this);
+                    });
+                }
+                return data !== undefined ? '<p>' + data + '</p>' : ' ';
+            },
+            fadeSpeed: 300,
+            onPopupClose: function () {
+                $body.removeClass('modal-active');
+            },
+            onPopupOpen: function () {
+                $body.addClass('modal-active');
+            },
+            overlayOpacity: 0,
+            popupCloserText: '',
+            popupHeight: 150,
+            popupLoaderText: '',
+            popupSpeed: 300,
+            popupWidth: 150,
+            selector: '.thumb > a.image',
+            usePopupCaption: true,
+            usePopupCloser: true,
+            usePopupDefaultStyling: false,
+            usePopupForceClose: true,
+            usePopupLoader: true,
+            usePopupNav: true,
+            windowMargin: 50
+        });
+
+        // Hack: Set margins to 0 when 'xsmall' activates.
+        skel
+            .on('-xsmall', function () {
+                $main[0]._poptrox.windowMargin = 50;
+            })
+            .on('+xsmall', function () {
+                $main[0]._poptrox.windowMargin = 0;
+            });
+
+        function getExifDataMarkup(img) {
+            var exif = fetchExifData(img);
+            var template = '';
+            for (var info in exif) {
+                if (info === "model") {
+                    template += '<i class="fa fa-camera-retro" aria-hidden="true"></i> ' + exif["model"] + '&nbsp;&nbsp;';
+                }
+                if (info === "aperture") {
+                    template += '<i class="fa fa-dot-circle-o" aria-hidden="true"></i> f/' + exif["aperture"] + '&nbsp;&nbsp;';
+                }
+                if (info === "shutter_speed") {
+                    template += '<i class="fa fa-clock-o" aria-hidden="true"></i> ' + exif["shutter_speed"] + '&nbsp;&nbsp;';
+                }
+                if (info === "iso") {
+                    template += '<i class="fa fa-info-circle" aria-hidden="true"></i> ' + exif["iso"] + '&nbsp;&nbsp;';
+                }
+            }
+            return template;
+        }
+
+        function fetchExifData(img) {
+            var exifData = {};
+
+            if (EXIF.getTag(img, "Model") !== undefined) {
+                exifData.model = EXIF.getTag(img, "Model");
+            }
+
+            if (EXIF.getTag(img, "FNumber") !== undefined) {
+                exifData.aperture = EXIF.getTag(img, "FNumber");
+            }
+
+            if (EXIF.getTag(img, "ExposureTime") !== undefined) {
+                exifData.shutter_speed = EXIF.getTag(img, "ExposureTime");
+            }
+
+            if (EXIF.getTag(img, "ISOSpeedRatings") !== undefined) {
+                exifData.iso = EXIF.getTag(img, "ISOSpeedRatings");
+            }
+            return exifData;
+        }
+
+    });
 
 })(jQuery);
